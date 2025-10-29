5ce175f507a56dc9f76f1da7a498e511b7a6583d
Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2025-10-29 05:18:37 -0500

    Add main class

---
+++ code/APPMIC-QuotingPlugins-customers/src/main/java/com/codesoft/ApplicationCustomer.java
@@ -0,0 +1,13 @@
+package com.codesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCustomer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCustomer.class, args);
+	}
+
+}
---
+++ code/APPMIC-QuotingPlugins-quotations/src/main/java/com/codesoft/ApplicationQuotation.java
@@ -0,0 +1,13 @@
+package com.codesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationQuotation {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationQuotation.class, args);
+	}
+
+}
