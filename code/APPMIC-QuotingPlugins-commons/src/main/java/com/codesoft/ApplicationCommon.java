8bfa13e3e1ae634373796dbbf3cf7a773b88817b
Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2025-10-29 05:09:59 -0500

    Restore src folders for gateway and reports

---
+++ code/APPMIC-QuotingPlugins-commons/src/main/java/com/codesoft/ApplicationCommon.java
@@ -0,0 +1,13 @@
+package com.codesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCommon {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCommon.class, args);
+	}
+
+}
---
+++ code/APPMIC-QuotingPlugins-gateway/src/main/java/com/codesoft/ApplicationGateway.java
@@ -0,0 +1,13 @@
+package com.codesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationGateway {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationGateway.class, args);
+	}
+
+}
