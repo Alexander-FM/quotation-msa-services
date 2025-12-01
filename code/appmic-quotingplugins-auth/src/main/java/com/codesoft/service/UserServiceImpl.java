37f541534b919b07b212f0cd0faa0e8bca5dde5f
Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2025-12-01 17:16:06 -0500

    feat: Add application configuration and WebClient factory for user authentication service

--- code/appmic-quotingplugins-auth/src/main/java/com/codesoft/service/UserServiceImpl.java
+++ code/appmic-quotingplugins-auth/src/main/java/com/codesoft/service/UserServiceImpl.java
@@ -2,51 +2,41 @@ package com.codesoft.service;
 
 import java.util.Objects;
 
+import com.codesoft.config.WebClientFactory;
 import com.codesoft.dto.UserResponseDto;
 import com.codesoft.exception.AuthMessageEnum;
 import com.codesoft.utils.AuthConstants;
 import com.codesoft.utils.GenericResponse;
 import com.codesoft.utils.WebClientErrorHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
-  private final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
-
-  private final WebClient.Builder loadBalancedWebClientBuilder;
-
-  private final WebClient simpleWebClient;
-
-  private final Environment env;
+  private final WebClient webClient;
 
   private final ObjectMapper objectMapper;
 
-  public UserServiceImpl(final WebClient.Builder loadBalancedWebClientBuilder, final WebClient simpleWebClient, final Environment env,
-    final ObjectMapper objectMapper) {
-    this.loadBalancedWebClientBuilder = loadBalancedWebClientBuilder;
-    this.simpleWebClient = simpleWebClient;
-    this.env = env;
+  public UserServiceImpl(final WebClientFactory webClientFactory, final ObjectMapper objectMapper) {
+    this.webClient = webClientFactory.retrieveWebClient(
+      AuthConstants.MS_AUTH_SERVICE,
+      AuthConstants.PORT_API_EMPLOYEE_USER_SERVICE
+    );
     this.objectMapper = objectMapper;
   }
 
   @Override
   public UserResponseDto validateUser(final String username, final String password) {
     try {
-      final WebClient client = getWebClient();
-      final String uri =
-        env.getProperty(AuthConstants.MS_AUTH_SERVICE, AuthConstants.PORT_API_EMPLOYEE_USER_SERVICE);
-      final GenericResponse<UserResponseDto> userResponseDto = client.get()
-        .uri(uri, uriBuilder -> uriBuilder
+      final GenericResponse<UserResponseDto> userResponseDto = webClient.get()
+        .uri("/user/loginByUsernameAndPassword", uriBuilder -> uriBuilder
           .queryParam("username", username)
           .queryParam("password", password)
           .build())
@@ -67,10 +57,8 @@ public class UserServiceImpl implements UserService {
   @Override
   public UserResponseDto findByUsername(String username) {
     try {
-      final WebClient client = getWebClient();
-      final String uri = env.getProperty(AuthConstants.MS_AUTH_SERVICE, AuthConstants.PORT_API_EMPLOYEE_USER_SERVICE);
-      final GenericResponse<UserResponseDto> userResponseDto = client.get()
-        .uri(uri, uriBuilder -> uriBuilder
+      final GenericResponse<UserResponseDto> userResponseDto = webClient.get()
+        .uri("/user/loginByUsername", uriBuilder -> uriBuilder
           .queryParam("username", username) // Solo enviamos username
           .build())
         .accept(MediaType.APPLICATION_JSON)
@@ -84,16 +72,6 @@ public class UserServiceImpl implements UserService {
       return null;
     } catch (final Exception ex) {
       throw WebClientErrorHandler.handle(ex, objectMapper, AuthMessageEnum.AUTH_EMPLOYEE_SERVICE_UNAVAILABLE);
-    }
-  }
-
-  private WebClient getWebClient() {
-    if (StringUtils.isBlank(env.getProperty(AuthConstants.MS_AUTH_SERVICE))) {
-      log.info("El uso de un cliente web simple como MS_AUTH_SERVICE no está configurado.");
-      return simpleWebClient;
-    } else {
-      log.info("Usando el WebClient del balanceador de carga.");
-      return loadBalancedWebClientBuilder.build();
     }
   }
 }
