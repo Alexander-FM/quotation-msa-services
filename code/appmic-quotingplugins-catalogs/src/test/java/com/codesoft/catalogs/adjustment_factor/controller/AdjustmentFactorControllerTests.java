3d50bbd25ab3e5bcfa415980d160f707198caa10
Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2025-12-03 01:03:51 -0500

    feat: Update security configuration in tests, enhance JWT handling, and adjust deployment settings for development and production

--- code/appmic-quotingplugins-catalogs/src/test/java/com/codesoft/catalogs/adjustment_factor/controller/AdjustmentFactorControllerTests.java
+++ code/appmic-quotingplugins-catalogs/src/test/java/com/codesoft/catalogs/adjustment_factor/controller/AdjustmentFactorControllerTests.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -66,7 +67,7 @@ class AdjustmentFactorControllerTests {
 
     when(this.adjustmentFactorService.findAll()).thenReturn(responseDtoList);
 
-    mockMvc.perform(get("/api/catalogs/adjustment-factor"))
+    mockMvc.perform(get("/api/catalogs/adjustment-factor").with(jwt()))
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.body").isArray())
       .andExpect(jsonPath("$.body[0].id").value(responseDtoList.getFirst().getId()))
@@ -78,7 +79,7 @@ class AdjustmentFactorControllerTests {
   @Test
   void retrieveByIdAdjustmentFactorsSuccessfullyTest() throws Exception {
     when(this.adjustmentFactorService.findById(1)).thenReturn(this.adjustmentFactorResponseDto);
-    mockMvc.perform(get("/api/catalogs/adjustment-factor/1"))
+    mockMvc.perform(get("/api/catalogs/adjustment-factor/1").with(jwt()))
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.body.id")
         .value(adjustmentFactorResponseDto.getId())).andReturn();
@@ -88,7 +89,7 @@ class AdjustmentFactorControllerTests {
   void retrieveByIdAdjustmentFactorsErrorTest() throws Exception {
     when(this.adjustmentFactorService.findById(10)).thenThrow(
       new AdjustmentFactorException(AdjustmentFactorMessageEnum.ADJUSTMENT_FACTOR_NOT_FOUND));
-    mockMvc.perform(get("/api/catalogs/adjustment-factor/10"))
+    mockMvc.perform(get("/api/catalogs/adjustment-factor/10").with(jwt()))
       .andExpect(status().isNotFound())
       .andExpect(jsonPath("$.body.code")
         .value(31404001)).andReturn();
@@ -101,7 +102,7 @@ class AdjustmentFactorControllerTests {
     when(this.adjustmentFactorService.create(any(AdjustmentFactorRequestDto.class)))
       .thenReturn(createResponseDto);
 
-    mockMvc.perform(MockMvcRequestBuilders.post("/api/catalogs/adjustment-factor")
+    mockMvc.perform(MockMvcRequestBuilders.post("/api/catalogs/adjustment-factor").with(jwt())
         .contentType(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(adjustmentFactorRequestDto)))
       .andExpect(status().isCreated())
@@ -113,7 +114,7 @@ class AdjustmentFactorControllerTests {
 
   @Test
   void createAdjustmentFactorWhenIdIsPresenteInTheRequestErrorTest() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.post("/api/catalogs/adjustment-factor")
+    mockMvc.perform(MockMvcRequestBuilders.post("/api/catalogs/adjustment-factor").with(jwt())
         .contentType(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(this.adjustmentFactorResponseDto)))
       .andExpect(status().isBadRequest())
@@ -127,7 +128,7 @@ class AdjustmentFactorControllerTests {
     when(adjustmentFactorService.findById(2)).thenReturn(createResponseDto);
     when(adjustmentFactorService.create(any(AdjustmentFactorRequestDto.class))).thenReturn(createResponseDto);
 
-    mockMvc.perform(MockMvcRequestBuilders.put("/api/catalogs/adjustment-factor/2")
+    mockMvc.perform(MockMvcRequestBuilders.put("/api/catalogs/adjustment-factor/2").with(jwt())
         .contentType(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(adjustmentFactorRequestDto)))
       .andExpect(status().isOk())
@@ -142,7 +143,7 @@ class AdjustmentFactorControllerTests {
     when(this.adjustmentFactorService.findById(999)).thenThrow(
       new AdjustmentFactorException(AdjustmentFactorMessageEnum.ADJUSTMENT_FACTOR_NOT_FOUND));
 
-    mockMvc.perform(MockMvcRequestBuilders.put("/api/catalogs/adjustment-factor/999")
+    mockMvc.perform(MockMvcRequestBuilders.put("/api/catalogs/adjustment-factor/999").with(jwt())
         .contentType(MediaType.APPLICATION_JSON)
         .content(this.objectMapper.writeValueAsString(this.adjustmentFactorRequestDto)))
       .andExpect(status().isNotFound())
@@ -152,7 +153,7 @@ class AdjustmentFactorControllerTests {
 
   @Test
   void updateAdjustmentFactorWhenIdIsZeroErrorTest() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.put("/api/catalogs/adjustment-factor/0")
+    mockMvc.perform(MockMvcRequestBuilders.put("/api/catalogs/adjustment-factor/0").with(jwt())
         .contentType(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(this.adjustmentFactorRequestDto)))
       .andExpect(status().isBadRequest())
@@ -163,7 +164,7 @@ class AdjustmentFactorControllerTests {
   void deleteAdjustmentFactorSuccessfullyTest() throws Exception {
     doNothing().when(adjustmentFactorService).deleteById(1);
 
-    mockMvc.perform(MockMvcRequestBuilders.delete("/api/catalogs/adjustment-factor/1"))
+    mockMvc.perform(MockMvcRequestBuilders.delete("/api/catalogs/adjustment-factor/1").with(jwt()))
       .andExpect(status().isNoContent())
       .andReturn();
   }
@@ -173,7 +174,7 @@ class AdjustmentFactorControllerTests {
     doThrow(new AdjustmentFactorException(AdjustmentFactorMessageEnum.ADJUSTMENT_FACTOR_NOT_FOUND)).when(adjustmentFactorService)
       .deleteById(10);
 
-    mockMvc.perform(MockMvcRequestBuilders.delete("/api/catalogs/adjustment-factor/10"))
+    mockMvc.perform(MockMvcRequestBuilders.delete("/api/catalogs/adjustment-factor/10").with(jwt()))
       .andExpect(status().isNotFound())
       .andExpect(jsonPath("$.body.code").value(31404001))
       .andReturn();
