088d13012bded21625066c5e0a3b55ee5c40a6e3
Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2025-12-11 20:53:15 -0500

    feat: Add module and material_module tables with sequences and constraints

--- code/appmic-quotingplugins-quotations/src/main/resources/db/quotations.sql
+++ code/appmic-quotingplugins-quotations/src/main/resources/db/quotations.sql
@@ -1,3 +1,98 @@
+-- =============== MODULE TYPE
+-- Create Sequence
+CREATE SEQUENCE module_id_seq
+    START WITH 1
+    INCREMENT BY 1;
+-- Table
+CREATE TABLE module
+(
+    id         INTEGER PRIMARY KEY   DEFAULT NEXTVAL('module_id_seq'),
+    name       VARCHAR(100) NOT NULL,
+    dimensions VARCHAR(500) NULL,
+    is_active  BOOLEAN      NOT NULL DEFAULT TRUE
+);
+-- Unique Values
+ALTER TABLE module
+    ADD CONSTRAINT module_name UNIQUE (name);
+-- Comments
+COMMENT
+ON TABLE module IS 'Esta tabla contiene información de los módulos por ejemplo, Basico 8N100';
+COMMENT
+ON COLUMN module.name IS 'Nombre del módulo';
+COMMENT
+ON COLUMN module.dimensions IS 'Descripción del módulo';
+COMMENT
+ON COLUMN module.is_active IS 'Verificar si está disponible el módulo';
+-- Insert
+INSERT INTO module (name, dimensions, is_active)
+VALUES ('Basico 8N100', 'Tamaño 230 cm * 110.2 cm * 49.3 cm', true),
+       ('Basico 8N80', 'Tamaño 230 cm * 85.8 cm * 49.3 cm', true),
+       ('Basico 8N60', 'Tamaño 230 cm * 59.8 cm * 49.3 cm', true);
+-- Query
+SELECT *
+FROM module;
+
+-- =============== MODULE CONCEPTS
+-- Create Sequence
+CREATE SEQUENCE module_concepts_id_seq
+    START WITH 1
+    INCREMENT BY 1;
+-- Table
+CREATE TABLE module_concepts
+(
+    id             INTEGER PRIMARY KEY DEFAULT NEXTVAL('module_concepts_id_seq'),
+    overheads_cost DECIMAL(5, 2) NOT NULL,
+    fee            DECIMAL(5, 2) NULL,
+    rebate         DECIMAL(5, 2) NULL,
+    profit_margin  DECIMAL(5, 2) NULL,
+    module_id      INTEGER NOT NULL,
+    CONSTRAINT fk_module_concepts_module FOREIGN KEY (module_id) REFERENCES module (id)
+);
+-- Comments
+COMMENT
+ON TABLE module_concepts IS 'Esta tabla contiene información general del módulo';
+COMMENT
+ON COLUMN module_concepts.overheads_cost IS 'Gastos generales';
+COMMENT
+ON COLUMN module_concepts.fee IS 'FEE';
+COMMENT
+ON COLUMN module_concepts.rebate IS 'Rebate';
+COMMENT
+ON COLUMN module_concepts.profit_margin IS 'Margen de utilidad';
+COMMENT
+ON COLUMN module_concepts.module_id IS 'Referencia al módulo';
+-- Query
+SELECT *
+FROM module_concepts;
+
+
+-- ==================== TABLA MATERIALS MODULE
+-- Create Sequence
+CREATE SEQUENCE material_module_id_seq
+    START WITH 1
+    INCREMENT BY 1;
+-- Table
+CREATE TABLE material_module
+(
+    id            INTEGER PRIMARY KEY DEFAULT NEXTVAL('material_module_id_seq'),
+    -- Referencia al otro micro servicio (Solo guardamos el ID)
+    module_id     INTEGER NOT NULL,
+    material_id   INTEGER NULL,
+    -- CANTIDAD BASE
+    quantity         DECIMAL(10, 2) NOT NULL, -- Ej.: 8.00 (cenefas) o 20.63 (kg)
+    -- PARÁMETROS DE CONFIGURACIÓN POR DEFECTO (La "Receta")
+    -- Estos valores se copiarán a la cotización cuando se elija este módulo
+    description_ref  VARCHAR(150),      -- Ej.: "105.0 x 5.0 - cenefas"
+    -- Opcionales (Dependen del tipo de material)
+    default_yield    INTEGER NULL,      -- Ej.: 35 (Para Planchas)
+    default_width    DECIMAL(10, 2) NULL, -- Ej: 75.0 (Para Poliestireno)
+    default_length   DECIMAL(10, 2) NULL  -- Ej: 30.0 (Para Poliestireno)
+    CONSTRAINT fk_material_module_module FOREIGN KEY (module_id) REFERENCES module (id)
+);
+-- Query
+SELECT *
+FROM material_module;
+
 -- ==================== TABLE QUOTATION
 CREATE TABLE quotation
 (
